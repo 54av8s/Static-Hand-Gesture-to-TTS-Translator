431ba397a427bd2c2283cd4241cb0192aae9d95d
Travis <travishunt120808@gmail.com>
2026-08-05 10:08:53 +0800

    Add files via upload

---
+++ scikitknnmodel.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+import joblib
+
+data = pd.read_csv("gesture_data.csv", header=None)
+X = data.iloc[:, 1:]  # Features (normalized coordinates)
+Y = data.iloc[:, 0]   # Labels (gestures)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+model = KNeighborsClassifier(n_neighbors=3)
+model.fit(X_train, Y_train)
+
+print("Model accuracy:", model.score(X_test, Y_test))
+joblib.dump(model, "gesture_knn_model.pkl")  # Save the trained model
+print("Model saved/updated")
